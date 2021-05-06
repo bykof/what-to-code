ab15ee224d2730d905ca48d57840c1a7d8a4a40f
Michael Bykovski <michael.bykovskii@aoe.com>
2021-05-06 14:35:59 +0200

    Adapt local development with db

--- backend/constants.py
+++ backend/constants.py
@@ -1,4 +1,4 @@
 import os
 
-SQLALCHEMY_URL = os.environ.get('SQLALCHEMY_URL', 'postgresql://localhost:5432/what_to_code')
+SQLALCHEMY_URL = os.environ.get('SQLALCHEMY_URL', 'postgresql://what_to_code@localhost:5432/what_to_code')
 REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost/0')
